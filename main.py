705475eb3f882d5e930dc81048dab177e07640bf
DanielEmirah <danielemirah@gmail.com>
2026-02-27 18:28:24 +0300

    Boucle d'évènements et Taille fenêtre

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ pygame.init()
 
 # Générer fenêtre
 pygame.display.set_caption("Commet Fall Game") # Titre de la fenêtre
-pygame.display.set_mode((500,300)) #Définition dimension fenêtre
+pygame.display.set_mode((1080,720)) #Définition dimension fenêtre
+
+#Boucle du jeu
+running = True
+while running:
+    #Si le joueur ferme la fenêtre
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #si fermeture de fenêtre
+            running = False
+            pygame.quit()
