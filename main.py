743ba57e0df059815e29926f987ac16d8de07a08
DanielEmirah <danielemirah@gmail.com>
2026-02-27 18:20:26 +0300

    Création fenêtre

---
+++ main.py
@@ -0,0 +1,8 @@
+import pygame
+
+# Charger les composants à l'intérieur de pygame
+pygame.init()
+
+# Générer fenêtre
+pygame.display.set_caption("Commet Fall Game") # Titre de la fenêtre
+pygame.display.set_mode((500,300)) #Définition dimension fenêtre
